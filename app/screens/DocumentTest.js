44340ff8100cc5969ac1230824869add4395b8a7
eelianap <e0559390@u.nus.edu>
2021-06-23 14:19:05 +0800

    Tested on Documents Screen

--- app/screens/DocumentTest.js
+++ app/screens/DocumentTest.js
@@ -1,81 +1,89 @@
-import React, {useState} from 'react';
-import {StyleSheet, Button, Text, Image, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Button, Text, Image, View, Platform, FlatList} from 'react-native';
+
 import DocumentPicker from "expo-document-picker/src/ExpoDocumentPicker";
 import * as FileSystem from 'expo-file-system';
-import PdfThumbnail from 'react-native-pdf-thumbnail';
 import {db, auth, fb} from '../config/Database';
 import * as IntentLauncher from "expo-intent-launcher";
 
-const DocumentTest = () => {
-    const [thumbnail, setThumbnail] = React.useState('');
-    const [error, setError] = React.useState('');
+const DocumentTest = ({navigation}) => {
     const [fileURI, setFileURI] = useState('');
+    const [fileName, setFileName] = useState('');
+    const [files, setFiles] = useState([]);
 
-    const reference = fb.child('test.pdf');
+    const loadFiles = async () => {
+        let docRef = db.collection("Files").doc(auth.currentUser.uid)
+        await docRef.get().then(doc => {
+            if (doc.exists) {
+                setFiles(doc.data().files);
+            }
+            return () => console.log("Done")
+        })
+    }
+
+    useEffect(() => {(async () => loadFiles())()},
+        []);
+
+    const file = {
+        fileName,
+        fileURI
+    }
+
+    const updateResponse = async (response) => {
+        if (response.type === 'success') {
+            setFileName(response.name);
+            await FileSystem.getContentUriAsync(response.uri)
+                .then(cUri => setFileURI(cUri));
+        }
+    }
 
     const onPress = async () => {
         try {
-            const response = await DocumentPicker.getDocumentAsync(
-                {type: 'application/pdf'});
+            await DocumentPicker
+                .getDocumentAsync({type: '*/*'})
+                .then(resp => updateResponse(resp));
 
-            await FileSystem.getContentUriAsync(response.uri).then(cUri => {
-                setFileURI(cUri);
-                /*
-                IntentLauncher.startActivityAsync('android.intent.action.VIEW', {
-                    data: cUri,
-                    flags: 1,
-                });
-                 */
-            });
-
-            const {uri} = await FileSystem.getInfoAsync(fileURI);
-            console.log(uri);
-            /*
-            await db.collection('Files')
-                .doc(auth.currentUser.uid)
-                .set({
-                    file: fileURI
-                }).then(content => console.log(content));
-             */
-
-            //const url = await reference.getDownloadURL();
-            const result = await PdfThumbnail.generate(uri, 0)
-            setThumbnail(result);
-            setError(undefined);
+            if (!files.includes(file)) {
+                await db.collection('Files')
+                    .doc(auth.currentUser.uid)
+                    .set({
+                        files: [...files, file]
+                    }).then(() => {
+                        setFileName('')
+                        setFileURI('')
+                    });
+            }
         } catch (err) {
-            setThumbnail(undefined);
-            setError(err);
+            console.log(err);
         }
     };
 
-    const thumbnailResult = thumbnail ? (
-        <>
-            <Image
-                source={thumbnail}
-                resizeMode="contain"
-                style={styles.thumbnailImage}
-            />
-            <Text style={styles.thumbnailInfo}>uri: {thumbnail.uri}</Text>
-            <Text style={styles.thumbnailInfo}>width: {thumbnail.width}</Text>
-            <Text style={styles.thumbnailInfo}>height: {thumbnail.height}</Text>
-        </>
-    ) : null;
-
-    const thumbnailError = error ? (
-        <>
-            <Text style={styles.thumbnailError}>Error code: {error.code}</Text>
-            <Text style={styles.thumbnailError}>Error message: {error.message}</Text>
-        </>
-    ) : null;
+    const preview = async () => {
+        try {
+            if (Platform.OS === 'android') {
+                await IntentLauncher.startActivityAsync('android.intent.action.VIEW', {
+                    data: fileURI,
+                    flags: 1,
+                });
+            } else {
+                navigation.navigate('PDF Preview', {fileURI: fileURI});
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.thumbnailPreview}>
-                {thumbnailResult}
-                {thumbnailError}
+            <View style={styles.container}>
+                <View style={{flex: 0}}>
+                    <FlatList data={files}
+                              keyExtractor={item => item.fileName}
+                              renderItem={({item}) => <Text>{item.fileName}</Text>} />
+                </View>
+
+                <Button onPress={onPress} title="Pick PDF File" />
+                <Button onPress={preview} title={"Show File"} />
             </View>
-            <Button onPress={onPress} title="Pick PDF File" />
-        </View>
     );
 };
 
@@ -87,20 +95,5 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         padding: 20,
-    },
-    thumbnailPreview: {
-        padding: 20,
-        alignItems: 'center',
-    },
-    thumbnailImage: {
-        width: 200,
-        height: 200,
-        marginBottom: 20,
-    },
-    thumbnailInfo: {
-        color: 'darkblue',
-    },
-    thumbnailError: {
-        color: 'crimson',
-    },
+    }
 });
---
+++ app/screens/PDFPreview.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from "react";
+import PDFReader from "rn-pdf-reader-js";
+import {View} from "react-native";
+import AppButton from "../container/AppButton";
+import {useNavigation} from "@react-navigation/native";
+import {auth, db} from "../config/Database";
+
+const PDFPreview = (props) => {
+    const [fileURI, setFileURI] = useState('');
+    const navigation = useNavigation();
+
+    const loadURI = async () => {
+        let docRef = db.collection("Files").doc(auth.currentUser.uid)
+        await docRef.get().then(doc => {
+            if (doc.exists) {
+                console.log(doc.data());
+                setFileURI(doc.data().file)
+            }
+            return () => console.log("Done")
+        })
+    }
+
+    useEffect(() => {(async () => loadURI())()},
+        []);
+
+    return (
+        <View style={{flex: 1}}>
+            <PDFReader source={{uri: fileURI}}/>
+            <AppButton title={"Done"} handler={() => navigation.navigate("Documents")} />
+        </View>
+    )
+}
+
+export default PDFPreview;
