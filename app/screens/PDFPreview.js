88d8ca887f8589dd00bdcdb40ac4ef256125456e
eelianap <e0559390@u.nus.edu>
2021-06-24 21:28:45 +0800

    Tested with uploading documents

--- app/screens/PDFPreview.js
+++ app/screens/PDFPreview.js
@@ -2,31 +2,12 @@ import React, {useEffect, useState} from "react";
 import PDFReader from "rn-pdf-reader-js";
 import {View} from "react-native";
 import AppButton from "../container/AppButton";
-import {useNavigation} from "@react-navigation/native";
-import {auth, db} from "../config/Database";
-import {FileSystem} from "react-native-unimodules";
 
-const PDFPreview = (props) => {
-    const [uri, setUri] = useState('');
-    const navigation = useNavigation();
-
-    const loadUri = async () => {
-        let docRef = db.collection("Files").doc(auth.currentUser.uid)
-        await docRef.get().then(async (doc) => {
-            if (doc.exists) {
-                await FileSystem.getContentUriAsync(doc.data.fileUris[props.id])
-                    .then(cUri => setUri(cUri))
-            }
-            return () => console.log("Done")
-        })
-        console.log(uri);
-    }
-
-    useEffect(() => {(async() => loadUri())()}, []);
+const PDFPreview = ({navigation, route}) => {
 
     return (
         <View style={{flex: 1}}>
-            <PDFReader source={{uri: uri}}/>
+            <PDFReader source={{uri: route.params.fileUri}}/>
             <AppButton title={"Done"} handler={() => navigation.navigate("Documents")} />
         </View>
     )
